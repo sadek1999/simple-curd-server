543d33743c74ffc5ddc884fc49458cffa1cb19a3
Sadek Ali <sadekali1530@gmail.com>
2023-10-17 22:23:24 +0600

    add the code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
  const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port= process.env.PORT || 5000;
 
@@ -42,6 +42,13 @@ async function run() {
         console.log('new user',user)
        const result=await userCollaction.insertOne(user);
        res.send(result);
+    })
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query= {_id: new ObjectId(id)};
+      const result =await userCollaction.deleteOne(query)
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
